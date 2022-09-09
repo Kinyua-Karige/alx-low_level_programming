28ea71766ac069a293de7e6e6d6acfca38529e72
Francis Kinyua <fkarige@gmail.com>
2022-09-08 17:20:17 -0700

    Task5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,17 +1,10 @@
-#include <stdio.h>                                                                                                                                                                              
-
-/**                                                                                                                                                                                             
-
-* main - Entry point                                                                                                                                                                            
-
-* this prints a statement                                                                                                                                                                       
-
-*Return: Always 0 (Success)                                                                                                                                                                     
-
-*/                                                                                                                                                                                              
-
-int main(void)                                                                                                                                                                                  
-
+#include <stdio.h>
+/**
+*main - Entry point
+*This script prints a statement
+*Return: Always 0 (Success)
+*/
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 
